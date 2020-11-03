5562908fe82cfe41965afbadcfaddbf872a1a1c1
igram <igram.jp@gmail.com>
2020-11-04 08:51:52 +0900

    as pattern

--- baby.hs
+++ baby.hs
@@ -52,3 +52,17 @@ second (_, y, _) = y
 
 third :: (a, b, c) -> c
 third (_, _, z) = z
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x : _) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x : []) = "The list has one element: " ++ show x
+tell (x : y : []) = "The element has two elements: " ++ show x ++ " and " ++ show y
+tell (x : y : _) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+firstLetter :: String -> String
+firstLetter "" = "Empty string, whoops!"
+firstLetter all@(x : xs) = "The first letter of " ++ all ++ " is " ++ [x]
