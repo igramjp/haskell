ea60afb1a4af073782cf2f3a0b4c7d08d28fecb6
igram <igram.jp@gmail.com>
2020-11-03 09:24:50 +0900

    pattern match

--- baby.hs
+++ baby.hs
@@ -19,8 +19,36 @@ addThree x y z = x + y + z
 factorial :: Integer -> Integer
 factorial n = product [1 .. n]
 
+factorial' :: Int -> Int
+factorial' 0 = 1
+factorial' n = n * factorial' (n - 1)
+
 circumference :: Float -> Float
 circumference r = 2 * pi * r
 
 circumference' :: Double -> Double
 circumference' r = 2 * pi * r
+
+lucky :: Int -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky _ = "Sorry, you're out of luck, pal!"
+
+sayMe :: Int -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe _ = "Not between 1 and 5"
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
