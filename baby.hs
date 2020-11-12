4eaadd167d31bad66cf92db3b3ec7d39cad560b3
igram <igram.jp@gmail.com>
2020-11-12 09:09:03 +0900

    5.1

--- baby.hs
+++ baby.hs
@@ -185,3 +185,19 @@ quicksort (x:xs) =
   let smallOrEqual = [a | a <- xs, a <= x]
       larger = [a | a <- xs, a > x]
   in quicksort smallOrEqual ++ [x] ++ quicksort larger
+
+multThree :: Int -> Int -> Int -> Int
+multThree x y z = x * y * z
+
+compareWithHundred :: Int -> Ordering
+compareWithHundred x = compare 100 x
+
+compareWithHundred' :: Int -> Ordering
+compareWithHundred' = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
